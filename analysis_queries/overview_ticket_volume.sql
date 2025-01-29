a210050a9470fba271e2968b898ebb0597073d21
Nick Feichtel <nfeichtel@gmail.com>
2025-01-28 20:16:46 -0700

    addded queries

--- analysis_queries/overview_ticket_volume.sql
+++ analysis_queries/overview_ticket_volume.sql
@@ -3,4 +3,31 @@ SELECT
     COUNT(*),
     ROUND(COUNT(*) / (SELECT COUNT(ticket_status)::numeric FROM support_tickets) * 100, 2) AS percent_status
 FROM support_tickets
-GROUP BY ticket_status
+GROUP BY ticket_status;
+
+SELECT 
+    ticket_channel,
+    COUNT(*),
+    ROUND(COUNT(*) / (SELECT COUNT(ticket_status)::numeric FROM support_tickets) * 100, 2) AS percent_status
+FROM support_tickets
+GROUP BY ticket_channel;
+
+SELECT 
+    ticket_type,
+    COUNT(*),
+    ROUND(COUNT(*) / (SELECT COUNT(ticket_status)::numeric FROM support_tickets) * 100, 2) AS percent_status
+FROM support_tickets
+GROUP BY ticket_type;
+
+
+SELECT
+    ticket_type,
+    ticket_channel,
+    COUNT(*),
+    ROUND(COUNT(*) / (SELECT COUNT(ticket_status)::numeric FROM support_tickets) * 100, 2) AS percent_status
+FROM support_tickets
+GROUP BY 
+    ticket_type,
+    ticket_channel
+ORDER BY
+    ticket_channel;
--- analysis_queries/ticket_timeline.sql
+++ analysis_queries/ticket_timeline.sql
@@ -1,5 +1,7 @@
 SELECT
     MIN(first_response_time) AS first_ticket_time,
     MAX(first_response_time) AS last_ticket_time,
-    ROUND(EXTRACT(EPOCH FROM MAX(first_response_time) - MIN(first_response_time)) / 3600, 2) AS ticket_timeline
+    ROUND(EXTRACT(EPOCH FROM MAX(first_response_time) - MIN(first_response_time)) / 3600, 2) AS ticket_timeline,
+    ROUND(COUNT(*) / ROUND(EXTRACT(EPOCH FROM MAX(first_response_time) - MIN(first_response_time)) / 3600, 2), 2) AS tickets_per_hour,
+    ROUND(COUNT(*) / ROUND(EXTRACT(EPOCH FROM MAX(first_response_time) - MIN(first_response_time)) / 3600, 2) / 65, 2) AS tickets_per_hour_per_rep
 FROM support_tickets
